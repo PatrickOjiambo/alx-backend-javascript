49ae1c1b3a18d6372ddfda42e26a07dd84116e02
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2024-01-04 00:44:16 +0300

    feat: Number 12

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,4 +1,10 @@
 export default function createReportObject(employeesList) {
-console.log(employeesList);
-return 1
+  const result = {
+    allEmployees: { ...employeesList },
+    getNumberOfDepartments(employeesList) {
+      return Object.keys(employeesList).length;
+    },
+  };
+
+  return result;
 }
