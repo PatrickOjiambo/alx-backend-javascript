941d4e833f774c2167f5619c06b6d2f2f621cb43
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2024-01-04 01:28:51 +0300

    feat: Number 0

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,6 +1,3 @@
 export default function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-      const fakeAPIResponse = { message: 'Hello from the API!' };
-      resolve(fakeAPIResponse);
-    });
+    return new Promise(() => {});
   }
