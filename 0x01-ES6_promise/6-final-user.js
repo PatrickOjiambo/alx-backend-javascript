639b8c3c39ec732136fda7e4cdee2157fbebdb58
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2024-01-05 14:50:49 +0300

    fix:Number 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -11,13 +11,13 @@ export default async function handleProfileSignup(
     const signup = await signUpUser(firstName, lastName);
     result.push({ status: 'fulfilled', value: signup });
   } catch (e) {
-    result.push({ status: 'rejected', value: e });
+    result.push({ status: 'rejected', value: e.tostring() });
   }
   try {
     const upload = await uploadPhoto(fileName);
     result.push({ status: 'fulfilled', value: upload });
   } catch (e) {
-    result.push({ status: 'rejected', value: e });
+    result.push({ status: 'rejected', value: e.toString() });
   }
 
   return result;
