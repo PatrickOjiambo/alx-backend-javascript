a7eb3f1e5862893359a33b9526aa17d1a19df42d
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2024-01-05 09:06:42 +0300

    feat:Number 3

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,12 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()])
+    .then((results) => {
+      console.log(
+        `${results[0].body} ${results[1].firstName} ${results[1].lastName}`,
+      );
+    })
+
+    .catch('Signup system offline');
+}
