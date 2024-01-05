45d2f73bb42fe9270f49f633f31387c3e247eb87
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2024-01-05 15:54:03 +0300

    fix:Number 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,13 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
-export default async function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
-    .then((results) => {
-      console.log(
-        `${results[0].body} ${results[1].firstName} ${results[1].lastName}`,
-      );
-    })
-    .catch(() => {
-      'Signup system offline';
-    });
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then(([body, fullName]) => console.log(`${body.body} ${fullName.firstName} ${fullName.lastName}`))
+    .catch(() => console.log('Signup system offline'));
 }
