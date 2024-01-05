c28101c8ce68de34ef4ffb0c1b6fa5ac7ab0be3e
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2024-01-05 14:46:11 +0300

    feat:Number 6

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,24 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default async function handleProfileSignup(
+  firstName,
+  lastName,
+  fileName,
+) {
+  let result = [];
+  try {
+    const signup = await signUpUser(firstName, lastName);
+    result.push({ status: 'fulfilled', value: signup });
+  } catch (e) {
+    result.push({ status: 'rejected', value: e });
+  }
+  try {
+    const upload = await uploadPhoto(fileName);
+    result.push({ status: 'fulfilled', value: upload });
+  } catch (e) {
+    result.push({ status: 'rejected', value: e });
+  }
+
+  return result;
+}
