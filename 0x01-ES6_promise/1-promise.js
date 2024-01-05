c88e575f58fbbf8d722c8b880e66d6b252ff1194
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2024-01-05 15:50:27 +0300

    fix:Number 1

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,8 +1,10 @@
 export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) {
+      console.log('Got a response from the API');
       resolve({ status: 200, body: 'Success' });
     } else {
+      console.log('Got a response from the API');
       reject(new Error('The fake API is not working currently'));
     }
   });
