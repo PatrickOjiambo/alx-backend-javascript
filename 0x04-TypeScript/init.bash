1fab083ec93ba2ec55a01df2f4226c06c5636010
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2024-01-11 10:33:11 +0300

    feat: task 9

---
+++ 0x04-TypeScript/init.bash
@@ -0,0 +1,2 @@
+touch package.json babel.config.js .eslintrc.js
+mkdir task_1 task_2 task_3 task_4 task_5
---
+++ 0x04-TypeScript/task_4/js/subjects/Java.ts
@@ -0,0 +1,15 @@
+namespace Subjects {
+    export interface Teacher {
+      experienceTeachingJava?: number;
+    }
+    export class Java extends Subject{
+      getRequirements() {
+        return 'Here is the list of requirements for Java';
+      }
+      getAvailableTeacher(){
+          if(!this.teacher.experienceTeachingReact) return 'No available teacher'
+          return `Available Teacher: ${this.teacher.firstName}`
+      }
+    }
+  }
+  
